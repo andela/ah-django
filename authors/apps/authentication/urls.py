844abeced11188c1bdb302e41f34483d69a9459c
misocho <misochobria@gmail.com>
2019-03-28 18:14:08 +0300

    164857215(password): reset password endpoint
    - update reset password url
    - update reset password confirm url
    
    [#164857215]

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^user/?$', UserRetrieveUpdateAPIView.as_view()),
     url(r'^users/?$', RegistrationAPIView.as_view()),
     url(r'^users/login/?$', LoginAPIView.as_view()),
-    url(r'^password-reset/?$', ResetPassword.as_view(), name='password_reset'),
-    url(r'^password-reset-confirm/?$', ResetPasswordConfirmView.as_view(),
+    url(r'^users/password-reset/?$', ResetPassword.as_view(), name='password_reset'),
+    url(r'^users/password-reset-confirm/?$', ResetPasswordConfirmView.as_view(),
         name='password_reset_confirm'),
 ]

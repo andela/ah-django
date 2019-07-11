31165d3a5f25bbaa1864ea2df8bcdd9a5f0fc4dd
Antony Muriithi <44572764+justMuriithi@users.noreply.github.com>
2019-07-11 23:51:43 +0300

    Update settings.py

--- authors/settings.py
+++ authors/settings.py
@@ -211,12 +211,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-CORS_ORIGIN_WHITELIST = (
-    '0.0.0.0:4000',
-    'localhost:4000',
-    'https://ah-fortem-staging.herokuapp.com',
-)
-
+# CORS_ORIGIN_WHITELIST = (
+#     '0.0.0.0:4000',
+#     'localhost:4000',
+#     'https://ah-fortem-staging.herokuapp.com',
+# )
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Tell Django about the custom `User` model we created. The string
 # `authentication.User` tells Django we are referring to the `User` model in
